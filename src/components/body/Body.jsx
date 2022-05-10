1e5d74611f81c5a83709a014adc38b4ecfcba8c9
Kevin López <kevinavs.2001@gmail.com>
2022-05-09 20:51:56 -0500

    Media Screen sm

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -11,32 +11,32 @@ export const Body = () => {
             <div className="">
                 <img src={homero} alt="" />
             </div>
-            <div className='h-0.5 bg-black mx-6' ></div>
+            <div className='h-0.5 bg-black mx-6 sm:mx-16' ></div>
             <div className='flex-col text-center my-4'>
-                <div className='mx-20 my-2 bg-orange-500 rounded-xl bg-white bg-clip-border border-1 border-solid rounded shadow-sm rounded-xl relative justify-center overflow-hidden'>
+                <div className='mx-20 my-2 sm:mx-36 bg-orange-500 rounded-xl'>
                     <button type="button" className='py-4 w-full h-full text-lg font-mono '>About Me</button>
                 </div>
-                <div className='mx-20 my-2 bg-orange-500 rounded-xl'>
+                <div className='mx-20 my-2 sm:mx-36 bg-orange-500 rounded-xl'>
                     <button type="button" className='py-4 w-full h-full text-lg font-mono'>Briefcase</button>
                 </div>
-                <div className='mx-20 my-2 bg-orange-500 rounded-xl'>
+                <div className='mx-20 my-2 sm:mx-36 bg-orange-500 rounded-xl'>
                     <button type="button" className='py-4 w-full h-full text-lg font-mono'>Projects</button>
                 </div>
             </div>
-            <div className='h-0.5 bg-black mx-6' ></div>
+            <div className='h-0.5 bg-black mx-6 sm:mx-16' ></div>
             <div className='flex justify-center'>
-                <a href="https://www.youtube.com/watch?v=Pyr3M303lXA">
-                    <img src={gitHub} alt="" className='w-48 h-48 ' />
+                <a href="https://github.com/KevinL0pez/tailwind.git">
+                    <img src={gitHub} alt="" className='w-48 h-48 sm:w-56 sm:h-56' />
                 </a>
             </div>
-            <div className='h-0.5 bg-black mx-6' ></div>
+            <div className='h-0.5 bg-black mx-6 sm:mx-16' ></div>
             <div className='flex justify-center mx-6 my-4'>
-                <img src={famSimpson} alt=""  className='w-80 h-42'/>
+                <img src={famSimpson} alt=""  className='w-80 sm:w-90 h-42'/>
             </div>
-            <div className='h-0.5 bg-black mx-6' ></div>
+            <div className='h-0.5 bg-black mx-6 sm:mx-16' ></div>
             <div className='flex w-full justify-center py-6 text-center items-center'>
-                <img src={perfil} alt="" className='rounded-full w-12 h-12 mx-2'/>
-                <p className='text-3xl font-semibold font-sans' >Kevin López</p>
+                <img src={perfil} alt="" className='rounded-full w-12 h-12 '/>
+                <p className='text-3xl font-semibold font-sans mx-2' >Kevin López</p>
             </div>
         </div>
     </>
